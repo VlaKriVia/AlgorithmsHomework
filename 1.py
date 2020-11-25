471122c06b65ee83b542855e029d978d169e62f6
SlavaKrilov <50460016+VlaKriVia@users.noreply.github.com>
2020-11-25 12:20:03 +0300

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-num = input("Введите трёхзначное число: ")
-a, b ,c = int(num[0]), int(num[1]), int(num[2])
-print(a + b + c)
-print(a * b * c)
-
-#https://app.diagrams.net/#G1g_bYs2jPVBIot-IVANFojWxjHNJoblbH
